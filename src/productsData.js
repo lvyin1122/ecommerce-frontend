7550b17ed57c6dd23a258a3ba195033146064987
lvyin1122 <khiwhyzqd@outlook.com>
2023-02-21 12:39:45 +0800

    update

--- src/productsData.js
+++ src/productsData.js
@@ -1,11 +1,11 @@
 const data = [
   {
     id: 0,
-    name: "React: From Beginner to Expert",
+    name: "The Complete React Course",
     author: "John Doe",
     price: 9.99,
     image:
-      "https://user-images.githubusercontent.com/42922524/219308694-ac62869b-5923-4f6b-b69a-98e87073a8c0.png",
+      "https://user-images.githubusercontent.com/42922524/220246860-1652072d-5b7f-44b6-8f34-021b0de4a60e.jpg",
   },
   {
     id: 1,
@@ -13,63 +13,63 @@ const data = [
     author: "John Smith",
     price: 9.99,
     image:
-      "https://user-images.githubusercontent.com/42922524/219308720-3c4a34dc-986c-4e3b-8e14-24e29dc07256.png",
+      "https://user-images.githubusercontent.com/42922524/220246869-fd742cb7-cd61-4b05-9b58-26ea501e51e1.jpg",
   },
   {
     id: 2,
-    name: "JavaScript: The Complete Guide",
+    name: "Modern Web Devleopment Masterclass",
     author: "Stephen Lee",
     price: 9.99,
     image:
-      "https://user-images.githubusercontent.com/42922524/219308762-837542df-7b95-4fdd-b804-dcdb053ea11a.png",
+      "https://user-images.githubusercontent.com/42922524/220246874-88591866-abf0-48f4-8244-1b1cc5c07782.jpg",
   },
   {
     id: 3,
-    name: "NodeJS Bootcamp",
+    name: "The Complete iOS Development Bootcamp",
     author: "Susan Williams",
     price: 13.99,
     image:
-      "https://user-images.githubusercontent.com/42922524/219308791-2899bd59-a0a2-4204-8806-7aad5c854828.jpeg",
+      "https://user-images.githubusercontent.com/42922524/220246884-2547e763-4ce9-4568-af23-06186db07810.jpg",
   },
   {
     id: 4,
-    name: "The Complete iOS Development Bootcamp",
+    name: "Operating Systems for Beginners",
     author: "Allen Brown",
     price: 9.99,
     image:
-      "https://user-images.githubusercontent.com/42922524/219308793-40499d53-663d-4f42-a5ef-9f2b8aa65bb4.png",
+      "https://user-images.githubusercontent.com/42922524/220246887-0b5842ed-963c-4ee2-b7d9-07420cea531f.jpg",
   },
   {
     id: 5,
-    name: "Python: Zero to Mastery",
+    name: "Database Management Systems in Depth",
     author: "Robert Simmons",
     price: 10.99,
     image:
-      "https://user-images.githubusercontent.com/42922524/219308843-3c710807-0b83-46e3-8615-0e0d885cfa2e.jpeg",
+      "https://user-images.githubusercontent.com/42922524/220246900-c0857e62-9d9e-4ea9-b6be-ab108aa22c88.jpg",
   },
   {
     id: 6,
-    name: "C++ Programming Language",
+    name: "Parallel Programming in Java",
     author: "Steven Bell",
     price: 13.99,
     image:
-      "https://user-images.githubusercontent.com/42922524/219308851-78999937-07e2-4f8e-9b50-71379b7136eb.jpeg",
+      "https://user-images.githubusercontent.com/42922524/220246903-32e00bfe-5088-4c91-aa6a-203bf6913ed5.jpg",
   },
   {
     id: 7,
-    name: "Parallel Programming in Java",
+    name: "C++ Programming: Complete Guide",
     author: "Jenny Morris",
     price: 9.99,
     image:
-      "https://user-images.githubusercontent.com/42922524/219308855-8c5616a5-6c2f-4ba7-81c8-e949c8047571.jpeg",
+      "https://user-images.githubusercontent.com/42922524/220246905-4dad4336-0981-4a81-8a8e-c88dc9d91af5.jpg",
   },
   {
     id: 8,
-    name: "Spring Framework Maserclass",
+    name: "Computer Science 101",
     author: "Rose Goodman",
     price: 10.99,
     image:
-      "https://user-images.githubusercontent.com/42922524/219308860-3047e1a0-59ce-4a34-b4e6-4c9e7726d1da.png",
+      "https://user-images.githubusercontent.com/42922524/220246908-ea897412-2c6e-44af-9bee-0079895572af.jpg",
   },
 ];
 export default data;
