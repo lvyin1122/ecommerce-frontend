8a290c144c7ad5bf11694af340204e992dd31557
lvyin1122 <khiwhyzqd@outlook.com>
2023-02-22 13:56:36 +0800

    update

--- src/productsData.js
+++ src/productsData.js
@@ -1,76 +1,75 @@
 const data = [
-    {
-      id: 0,
-      name: "React: From Beginner to Expert",
-      author: "John Doe",
-      price: 9.99,
-      image:
-        "https://cdn.thenewstack.io/media/2022/05/600b72f9-react-1024x680.png",
-    },
-    {
-      id: 1,
-      name: "Neural Networks from Scratch",
-      author: "John Smith",
-      price: 9.99,
-      image:
-        "https://7wdata.be/wp-content/uploads/2020/11/What-is-an-Artificial-Neural-Networks.png",
-    },
-    {
-      id: 2,
-      name: "JavaScript: The Complete Guide",
-      author: "Stephen Lee",
-      price: 9.99,
-      image:
-        "https://repository-images.githubusercontent.com/229449376/3cfde400-5298-11ea-9f39-aab161ef8f69",
-    },
-    {
-      id: 3,
-      name: "NodeJS Bootcamp",
-      author: "Susan Williams",
-      price: 13.99,
-      image:
-        "https://www.hkcodingclub.com/wp-content/uploads/2022/02/share-nodejs-logo-1024x536.png",
-    },
-    {
-      id: 4,
-      name: "The Complete iOS Development Bootcamp",
-      author: "Allen Brown",
-      price: 9.99,
-      image: "https://miro.medium.com/max/730/1*ND2d6CvH-Cz0dp5I_tYalQ.png",
-    },
-    {
-      id: 5,
-      name: "Python: Zero to Mastery",
-      author: "Robert Simmons",
-      price: 10.99,
-      image:
-        "https://bytesofintelligence.co.uk/wp-content/uploads/2021/06/python.jpeg",
-    },
-    {
-      id: 6,
-      name: "C++ Programming Language",
-      author: "Steven Bell",
-      price: 13.99,
-      image:
-        "https://res.cloudinary.com/practicaldev/image/fetch/s--Tu2z9cvG--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://miro.medium.com/max/696/1%2A6UpwEDOw04H5fKyaMGXpSw.png",
-    },
-    {
-      id: 7,
-      name: "Parallel Programming in Java",
-      author: "Jenny Morris",
-      price: 9.99,
-      image:
-        "https://mindqsystems.com/wp-content/uploads/2019/08/Core-Java-Training.jpg",
-    },
-    {
-      id: 8,
-      name: "Spring Framework Maserclass",
-      author: "Rose Goodman",
-      price: 10.99,
-      image:
-        "https://spring.io/images/OG-Spring.png",
-    },
-  ];
-  
-  export default data;
-  
+  {
+    id: 0,
+    name: "The Complete React Course",
+    author: "John Doe",
+    price: 9.99,
+    image:
+      "https://user-images.githubusercontent.com/42922524/220246860-1652072d-5b7f-44b6-8f34-021b0de4a60e.jpg",
+  },
+  {
+    id: 1,
+    name: "Neural Networks from Scratch",
+    author: "John Smith",
+    price: 9.99,
+    image:
+      "https://user-images.githubusercontent.com/42922524/220246869-fd742cb7-cd61-4b05-9b58-26ea501e51e1.jpg",
+  },
+  {
+    id: 2,
+    name: "Modern Web Devleopment Masterclass",
+    author: "Stephen Lee",
+    price: 9.99,
+    image:
+      "https://user-images.githubusercontent.com/42922524/220246874-88591866-abf0-48f4-8244-1b1cc5c07782.jpg",
+  },
+  {
+    id: 3,
+    name: "The Complete iOS Development Bootcamp",
+    author: "Susan Williams",
+    price: 13.99,
+    image:
+      "https://user-images.githubusercontent.com/42922524/220246884-2547e763-4ce9-4568-af23-06186db07810.jpg",
+  },
+  {
+    id: 4,
+    name: "Operating Systems for Beginners",
+    author: "Allen Brown",
+    price: 9.99,
+    image:
+      "https://user-images.githubusercontent.com/42922524/220246887-0b5842ed-963c-4ee2-b7d9-07420cea531f.jpg",
+  },
+  {
+    id: 5,
+    name: "Database Management Systems in Depth",
+    author: "Robert Simmons",
+    price: 10.99,
+    image:
+      "https://user-images.githubusercontent.com/42922524/220246900-c0857e62-9d9e-4ea9-b6be-ab108aa22c88.jpg",
+  },
+  {
+    id: 6,
+    name: "Parallel Programming in Java",
+    author: "Steven Bell",
+    price: 13.99,
+    image:
+      "https://user-images.githubusercontent.com/42922524/220246903-32e00bfe-5088-4c91-aa6a-203bf6913ed5.jpg",
+  },
+  {
+    id: 7,
+    name: "C++ Programming: Complete Guide",
+    author: "Jenny Morris",
+    price: 9.99,
+    image:
+      "https://user-images.githubusercontent.com/42922524/220246905-4dad4336-0981-4a81-8a8e-c88dc9d91af5.jpg",
+  },
+  {
+    id: 8,
+    name: "Computer Science 101",
+    author: "Rose Goodman",
+    price: 10.99,
+    image:
+      "https://user-images.githubusercontent.com/42922524/220246908-ea897412-2c6e-44af-9bee-0079895572af.jpg",
+  },
+];
+export default data;
