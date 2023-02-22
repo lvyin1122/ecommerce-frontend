90f4ae495106156b0f9b2e17757600303e12c50d
lvyin1122 <khiwhyzqd@outlook.com>
2023-02-22 14:10:27 +0800

    update

--- src/examples/Counter.js
+++ src/examples/Counter.js
@@ -2,13 +2,16 @@ import React from "react";
 import { useState } from "react";
 
 function Counter() {
+  // Initilize the count state with 0
   const [count, setCount] = useState(0);
 
   const clickHandler = () => {
-    setCount(prevState => prevState + 1);
+    // Update the count state by adding 1
+    setCount(prevState =>  prevState + 1);
   };
 
   return (
+    // Add clickHandler to the button
     <button onClick={clickHandler}>You have clicked {count} times.</button>
   );
 }
